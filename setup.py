963c8a40837cd74ddabed2c8c3d55ac3d149e3e6
Stuart Golodetz <sgolodetz@gxstudios.net>
2022-07-04 13:50:12 +0100

    Continue working on README files.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="smg-openni",
@@ -9,7 +9,7 @@ setup(
     author="Stuart Golodetz",
     author_email="stuart.golodetz@cs.ox.ac.uk",
     description="Interface for OpenNI cameras",
-    long_description="",  #long_description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sgolodetz/smg-openni",
     packages=find_packages(include=["smg.openni"]),
